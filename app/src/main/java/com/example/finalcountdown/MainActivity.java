672e26bfb56e92c51767f15f68f472fe2b48e37e
Jukka Lehtniemi <jil@omoroi.fi>
2022-05-26 22:47:06 +0300

    add sound files

--- app/src/main/java/com/example/finalcountdown/MainActivity.java
+++ app/src/main/java/com/example/finalcountdown/MainActivity.java
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
+import android.util.SparseIntArray;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.time.Clock;
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Locale;
@@ -26,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextViewCountDown;
     private Button mButtonSet;
 
+    private SoundPool soundPool;
+    //private int sound1;
+    private SparseIntArray sounds;
+
     private CountDownTimer mCountDownTimer;
     private boolean mTimerRunning;
     private long mEndTime;
@@ -34,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         mEditTextHour = findViewById(R.id.edit_text_hour);
         mEditTextMinute = findViewById(R.id.edit_text_minute);
@@ -78,9 +86,37 @@ public class MainActivity extends AppCompatActivity {
 
                 mTextViewCountDown.setTextColor(Color.BLACK);
                 setTime(inputMillis);
+                loadSounds();
                 startTimer();
             }
         });
+    }
+
+    private void loadSounds() {
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                .build();
+        soundPool = new SoundPool.Builder().setAudioAttributes(audioAttributes).build();
+        sounds = new SparseIntArray();
+        sounds.put(300, soundPool.load(this, R.raw.min5, 1));
+        sounds.put(240, soundPool.load(this, R.raw.min4, 1));
+        sounds.put(60, soundPool.load(this, R.raw.min1, 1));
+        sounds.put(50, soundPool.load(this, R.raw.sec50, 1));
+        sounds.put(40, soundPool.load(this, R.raw.sec40, 1));
+        sounds.put(30, soundPool.load(this, R.raw.sec30, 1));
+        sounds.put(20, soundPool.load(this, R.raw.sec20, 1));
+        sounds.put(10, soundPool.load(this, R.raw.sec10, 1));
+        sounds.put(9, soundPool.load(this, R.raw.sec9, 1));
+        sounds.put(8, soundPool.load(this, R.raw.sec8, 1));
+        sounds.put(7, soundPool.load(this, R.raw.sec7, 1));
+        sounds.put(6, soundPool.load(this, R.raw.sec6, 1));
+        sounds.put(5, soundPool.load(this, R.raw.sec5, 1));
+        sounds.put(4, soundPool.load(this, R.raw.sec4, 1));
+        sounds.put(3, soundPool.load(this, R.raw.sec3, 1));
+        sounds.put(2, soundPool.load(this, R.raw.sec2, 1));
+        sounds.put(1, soundPool.load(this, R.raw.sec1, 1));
+        sounds.put(0, soundPool.load(this, R.raw.start, 1));
     }
 
     private void setTime(long milliseconds) {
@@ -103,21 +139,36 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
         mTimerRunning = true;
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         updateWatchInterface();
     }
 
     private void pauseTimer() {
-        mCountDownTimer.cancel();
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+        }
         mTimerRunning = false;
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         updateWatchInterface();
     }
 
+    private void playSound(int id) {
+        Log.i("TAG", "playing sound " + id);
+        soundPool.play(id, 1, 1, 0, 0, 1);
+    }
+
     private void updateCountDownText() {
+        String prefix = "";
         long timeLeftInMillis = mEndTime - System.currentTimeMillis();
         int timeLeftInSecs = (int) timeLeftInMillis / 1000;
+        if (timeLeftInSecs < -60) {
+            pauseTimer();
+            return;
+        }
         if (timeLeftInSecs < 0) {
             mTextViewCountDown.setTextColor(android.graphics.Color.RED);
             timeLeftInSecs = -timeLeftInSecs;
+            prefix = "-";
         }
         int hours = timeLeftInSecs / 3600;
         int minutes = timeLeftInSecs % 3600 / 60;
@@ -128,7 +179,15 @@ public class MainActivity extends AppCompatActivity {
         } else {
             timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
         }
-        mTextViewCountDown.setText(timeLeftFormatted);
+        mTextViewCountDown.setText(prefix + timeLeftFormatted);
+
+        if (null != sounds){
+            int id = sounds.get(timeLeftInSecs, -1);
+            if (id != -1) {
+                playSound(id);
+                sounds.delete(timeLeftInSecs); // avoid repeat; todo: is this recoverable
+            }
+        }
     }
 
     private void updateWatchInterface() {
